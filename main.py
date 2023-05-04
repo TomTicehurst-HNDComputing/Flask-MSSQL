48b42ac76a33a966b48747d8427aba94d3926b2d
tticehurst <shnopy319@gmail.com>
2023-05-04 21:59:33 +0100

    removed unused import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, make_response, session
+from flask import Flask, render_template, request, redirect, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from dotenv import load_dotenv
